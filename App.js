aaa41f6cc4d03302777ef3e90b5aa7ef90259719
Rehan Fazal <rf26538@gmail.com>
2025-08-17 01:27:27 +0530

    Functional component and composition of component

--- App.js
+++ App.js
@@ -2,22 +2,32 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 
-const AppLayout = () => {
+const Header = () => {
     return (
-        <div className="heading">
+        <div className="header">
                 <div className="logo-container">
-                    <img className="logo" src="./logo.png" />   
+                    <img className="logo" src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png?nwm=1&nws=1&industry=food&sf=&txt_keyword=All" />   
                 </div>
             <div className="nav-items">
                 <ul>
                     <li>Home</li>
                     <li>About Us</li>
                     <li>Contact Us</li>
+                    <li>Cart</li>
                 </ul>
             </div>
         </div>
     )
 }
+
+const AppLayout = () => {
+    return (
+        <>
+            <Header />
+        </>
+    )
+}
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 
