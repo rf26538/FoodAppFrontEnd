3b38d3a199c07de45b1e46308675db1ce4fa928f
Rehan Fazal <rf26538@gmail.com>
2025-08-17 14:27:27 +0530

    make restaurant card

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-
 const Header = () => {
     return (
         <div className="header">
@@ -20,10 +19,40 @@ const Header = () => {
     )
 }
 
+const RestaurantCard = () => {
+    return (
+        <div className="res-card">
+            <img alt="res-logo" className="res-logo" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/vkhcohhmqfczycw9vsar"/>
+            <h3>Hotel Sai Nath & Sai Restaurant</h3>
+            <h4>North Indian, South Indian</h4>
+            <h4>4.4 stars</h4>
+            <h4>38 minutes</h4>
+        </div>
+    )
+}
+
+const Body = () => {
+    return (
+        <div className="body">
+            <div className="search">Search</div>
+            <div className="res-container">
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+            </div>
+        </div>  
+    )
+
+}
+
 const AppLayout = () => {
     return (
         <>
             <Header />
+            <Body/>
         </>
     )
 }
