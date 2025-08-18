793348e0b59f90d2061cb17c7dfefd66ad296ed5
Rehan Fazal <rf26538@gmail.com>
2025-08-19 04:37:35 +0530

    Class based components

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,13 @@
+import User from "./User";
+import UserClass from "./UserClass";
+
 const About  = () => {
     return (
         <div className="about">
             <h1>About page</h1>
             <h1>This is about page..</h1>
+            <User name={"Rehan Fazal (function)"}/>
+            <UserClass />
         </div>
     )
 }
---
+++ src/components/UserClass.jsx
@@ -0,0 +1,17 @@
+
+import React from "react";
+
+class UserClass extends React.Component {
+
+    render() {
+        return (
+        <div className='user-card'>
+        <h2>Name : Aman fazal</h2>
+        <h3>Location : Bihar</h3>
+        <h4>Contact : @amanfazal002</h4>
+        </div>
+        ) 
+    }
+}
+
+export default UserClass
