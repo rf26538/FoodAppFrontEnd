c4a0e69849d75fc1d7fb45a2a27b0066abfc30dd
Rehan Fazal <rf26538@gmail.com>
2025-08-19 00:47:32 +0530

    Show error with errorElement

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,7 +1,13 @@
+import { useRouteError } from "react-router";
+
 const Error  = () => {
+    const err = useRouteError();
+    console.log(err);
+    
     return (
         <div>
             <h1>Oops..</h1>
+            <h1>{err.status} Not Found</h1>
         </div>
     )
 }
