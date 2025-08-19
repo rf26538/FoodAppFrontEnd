1507e675912238294555965478230bf6a1ae441c
Rehan Fazal <rf26538@gmail.com>
2025-08-19 19:13:05 +0530

    Class file moved to forRef folder

--- forRef/UserClass.jsx
+++ forRef/UserClass.jsx
@@ -15,11 +15,8 @@ class UserClass extends React.Component {
     } 
 
     async componentDidMount() {
-
         const data = await fetch("https://api.github.com/users/akshaymarch7");
-
         const json = await data.json();
-
         console.log(json);
         
         this.setState({
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,14 +1,12 @@
-import UserClass from "./UserClass";
+// import UserClass from "./UserClass";
+import User from "./User";
 
 const About  = () => {
     return (
         <div className="about">
             <h1>About page</h1>
             <h1>This is about page..</h1>
-            {/* <User name={"Rehan Fazal (function)"}/> 
-            <UserClass name={"First (class)"} />
-            */}
-            <UserClass />
+            <User name={"Rehan Fazal (function)"}/> 
         </div>
     )
 }
